5be67a1e9fb399543f8e7be8f34d22af932a9b53
Theo Fahrizal Syam <theofahrizals@gmail.com>
2023-01-25 22:55:01 +0700

    add meet 9

---
+++ meet9/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Admin's Page</title>
+</head>
+<body>
+    <h1>Dafta Mahasiswa</h1>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Foto</th>
+            <th>Nama</th>
+            <th>Nim</th>
+            <th>E-mail</th>
+            <th>Program Studi</th>
+            <th>Tools</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>
+                <img src="" alt="foto siswa">
+            </td>
+            <td>ajeng</td>
+            <td>2420244</td>
+            <td>ajeng@gmail.com</td>
+            <td>Sastra</td>
+            <td>
+                <a href="#">Edit</a> | <a href="#">Delete</a>
+            </td>
+        </tr>
+    </table>
+</body> 
+</html>
