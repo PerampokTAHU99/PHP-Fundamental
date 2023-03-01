24e891e66a00861c19392526dcf21a713d74dabd
“perampoktahu99” <theofahrizals@gmail.com>
2023-03-01 08:23:53 +0000

    tst

--- meet9/index.php
+++ meet9/index.php
@@ -8,7 +8,7 @@
     <title>Admin's Page</title>
 </head>
 <body>
-    <h1>Dafta Mahasiswa</h1>
+    <h1>Daftar Mahasiswa</h1>
     <table border="1" cellpadding="10" cellspacing="0">
         <tr>
             <th>No.</th>
