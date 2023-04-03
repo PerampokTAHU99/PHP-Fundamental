d3ec3f799186bd8cf8c9bbfe0b95f7da9e2812ac
“perampoktahu99” <theofahrizals@gmail.com>
2023-04-03 22:47:30 +0700

    update form

--- meet10/addData.php
+++ meet10/addData.php
@@ -4,24 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/meet10/style.css">
     <title>Tambah Data Mahasiswa</title>
 </head>
 <body>
     <h1>Tambah Data Mahasiswa</h1>
-    <div class="addDataForm">
-        <form action="" class="formData">
-            <label for="">NIM :</label><br>
-            <input type="text"><br>
-            <label for="">Nama :</label><br>
-            <input type="text"><br>
-            <label for="">Email :</label><br>
-            <input type="text"><br>
-            <label for="">Program Studi :</label><br>
-            <input type="text"><br>
-            <label for="">Foto :</label><br>
-            <input type="text"><br><br>
-            <input type="submit" value="Tambah Data!">
+    <div class="addDataForm" >
+        <form action="" method="post" class="formData" >
+            <label for="nim">NIM :</label>
+            <input type="text" name="nim" id="nim"><br><br>
+            <label for="name">Nama :</label>
+            <input type="text" name="name" id="name"><br><br>
+            <label for="email">Email :</label>
+            <input type="text" name="email" id="email"><br><br>
+            <label for="programStudy">Program Studi :</label>
+            <input type="text" name="programStudy" id="programStudy"><br><br>
+            <label for="picture">Foto :</label>
+            <input type="text" name="picture" id="picture"><br><br>
+            <button type="submit" name="submit">Tambah Data!</button>
         </form>
     </div>
 </body>
