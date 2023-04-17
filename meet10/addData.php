242181e4e40a8dd83a73b486398cafbcfe4ea077
“perampoktahu99” <theofahrizals@gmail.com>
2023-04-17 23:55:30 +0700

    update

--- meet10/addData.php
+++ meet10/addData.php
@@ -1,4 +1,31 @@
 <!DOCTYPE html>
+<?php
+    //connection
+    $conn = mysqli_connect("localhost", "root", "", "fundamental_php");
+
+    //check submit button
+    if(isset($_POST["submit"])){
+        //fecth data from for every elemen in the form
+        $name = $_POST["name"];
+        $nim = $_POST["nim"];
+        $email = $_POST["email"];
+        $programStudy = $_POST["programStudy"];
+        $picture = $_POST["picture"];
+
+    //query
+    $query = "INSERT INTO mahasiswa (name, nim, email, programStudy, picture)VALUES ('$name','$nim','$email','$programStudy','$picture')";
+    mysqli_query($conn, $query);
+
+    //checking submit
+    if(mysqli_affected_rows($conn)>0){
+        echo "berhasil";
+    }else{
+        echo "gagal";
+        echo "<br>";
+        echo mysqli_error($conn);
+    }
+    };
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,11 +37,11 @@
 <body>
     <h1>Tambah Data Mahasiswa</h1>
     <div class="addDataForm" >
-        <form action="" method="post" class="formData" >
-            <label for="nim">NIM :</label>
-            <input type="text" name="nim" id="nim"><br><br>
+        <form action="" method="POST" class="formData" >
             <label for="name">Nama :</label>
             <input type="text" name="name" id="name"><br><br>
+            <label for="nim">NIM :</label>
+            <input type="text" name="nim" id="nim"><br><br>
             <label for="email">Email :</label>
             <input type="text" name="email" id="email"><br><br>
             <label for="programStudy">Program Studi :</label>
